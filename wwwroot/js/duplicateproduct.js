6bfd4d46a07f7821fa7d55130daa5ae7812af902
Mahmoud ElBetar <melbetar58@gmail.com>
2022-09-25 01:17:28 +0200

    Shipping System

---
+++ wwwroot/js/duplicateproduct.js
@@ -0,0 +1,9 @@
+﻿let row = document.getElementById("productRow");
+let dup_btn = document.getElementById("duplicate");
+let tbody = document.getElementById("tbody");
+let removeRow = document.getElementById("removeRow");
+
+dup_btn.onclick = function () {
+    let dup_row = row.cloneNode(true);
+    tbody.appendChild(dup_row);
+}
